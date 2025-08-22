15f5035f93d6d5b44503c23d2e1ccfbce830133c
benbarraclough <32736663+benbarraclough@users.noreply.github.com>
2025-08-22 12:14:58 +0100

    Reorder integrations in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://benbarraclough.github.io/BeerGoggleGames',
-  base: '/BeerGoggleGames/',
-  integrations: [tailwind(), sitemap(), mdx()]
+  base: '/BeerGoggleGames',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
