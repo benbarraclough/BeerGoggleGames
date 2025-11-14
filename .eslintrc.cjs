3ae7775286c0c70f67e9bcceb574f196cde9425a
Ben Barraclough <ben.barra@sky.com>
2025-11-14 09:37:45 +0000

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,3 @@
-/* eslint-env node */
-module.exports = {
-  root: true,
-  ignorePatterns: [
-    'dist/',
-    '.astro/',
-    'node_modules/'
-  ],
-  overrides: [
-    {
-      files: ['**/*.astro'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-        sourceType: 'module'
-      },
-      extends: [
-        'plugin:astro/recommended'
-      ],
-      rules: {}
-    },
-    {
-      files: ['**/*.{ts,tsx,js,jsx}'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
-      ],
-      rules: {
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
-      }
-    }
-  ]
-};
+/* Legacy ESLint config (unused). The project uses eslint.config.js (flat config).
+   Keeping this empty file only to avoid confusion if tools search for it. */
+module.exports = {};
